cc75e12d01742b8e11217f3ffcf5b5d53259ef9b
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2024-09-28 16:49:54 -0400

    Added search bar animation when pressed changes width

---
+++ ui.js
@@ -0,0 +1,16 @@
+const searchBar= document.getElementById("search-bar");
+
+searchBar.addEventListener('focus', () =>{
+
+    searchBar.style.width = "744px";
+    searchBar.style.outline = "none";
+    searchBar.style.borderColor = "#E87646";
+    searchBar.style.transition = "width 0.2s";
+});
+
+searchBar.addEventListener('blur', () => {
+
+    searchBar.style.width = "300px";
+    searchBar.style.outline = "none";
+    searchBar.style.borderColor = "#2C2C2C";
+});
