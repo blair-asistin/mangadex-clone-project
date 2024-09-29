44ed3b1c25bc9cd7ca4fca06fbbc7324a31bee59
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2024-09-28 22:58:38 -0400

    Change borderto outline when pressing search bar

--- ui.js
+++ ui.js
@@ -3,8 +3,7 @@ const searchBar= document.getElementById("search-bar");
 searchBar.addEventListener('focus', () =>{
 
     searchBar.style.width = "744px";
-    searchBar.style.outline = "none";
-    searchBar.style.borderColor = "#E87646";
+    searchBar.style.outline = "1px solid #E87646";
     searchBar.style.transition = "width 0.2s";
 });
 
@@ -13,4 +12,5 @@ searchBar.addEventListener('blur', () => {
     searchBar.style.width = "300px";
     searchBar.style.outline = "none";
     searchBar.style.borderColor = "#2C2C2C";
+    
 });
