5fdb5a6c4e3935e1aedfe7c9326b93621af8e27f
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2024-10-13 20:26:20 -0400

    Added Swiper.js component

--- ui.js
+++ ui.js
@@ -142,3 +142,28 @@ carouselSlide.addEventListener('transitionend', () =>{
 
 
 });
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
